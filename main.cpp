10ab2591b8dc7976898aa40b400d185bc71b01bb
alexanderblesener913-alt <alexanderblesener913@gmail.com>
2026-02-27 19:24:29 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+using namespace transform(var)
+  {
+  var statement = "english" + english.true;
+var sarcasm = "english" + english.(!true);
+  var question = "english" + english.(!true);
+  }
+int main(var)
+{
+  if (!question)
+  {
+    answer;
+    return 0;
+  }
+  var array[communicating] == var {statement, sarcasm, question};
+}
+int answer(!question)
+{
+  "answerquestion.js";
+}
