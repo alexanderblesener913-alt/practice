8f1ff1516b9e1c81746f26b5d47096185180fb1d
alexanderblesener913-alt <alexanderblesener913@gmail.com>
2026-02-27 19:32:07 -0600

    Create AI.h

---
+++ AI.h
@@ -0,0 +1,9 @@
+using main();
+transform() *class -();
+int namespace() -quesiton,object;
+class object(var main.namespace)[
+var main.class;
+var transform question;
+var object.("main.cpp").question = ("main.cpp").answer;
+var transform object = class object = main object = var object = answer object = question object;
+]
